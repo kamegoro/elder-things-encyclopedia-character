b8358afbef45c027596f2ee9e6760a650e2ab81e
kamegoro <tentyo0620@icloud.com>
2024-10-24 00:58:16 +0900

    init: spring

---
+++ src/test/java/com/example/elder_things_encyclopedia_character/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elder_things_encyclopedia_character;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
