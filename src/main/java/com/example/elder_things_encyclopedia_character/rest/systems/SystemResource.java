366908e3018168318ab3f885da701dd7593bcd77
kamegoro <tentyo0620@icloud.com>
2024-10-24 23:28:45 +0900

    add: system ping pong

---
+++ src/main/java/com/example/elder_things_encyclopedia_character/rest/systems/SystemResource.java
@@ -0,0 +1,13 @@
+package com.example.elder_things_encyclopedia_character.rest.systems;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SystemResource {
+   @RequestMapping(value = "/systems/ping", method = RequestMethod.GET)
+   public String ping() {
+       return "pong";
+   }
+}
